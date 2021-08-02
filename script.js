c04046f2ddb556cc1de285a9924d85aacf2cfea6
lkp25 <lkp25@o2.pl>
2021-08-02 13:46:47 +0200

    added function to store picked date on the button and display crrent month

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-import {format} from 'date-fns'
+import {format, fromUnixTime, getUnixTime} from 'date-fns'
 
 const datePickerBtn = document.querySelector('.openclose-btn')
+const datePickerHeaderText = document.querySelector('.month-year')
 const calendar = document.querySelector('.date-picker-box')
 
 datePickerBtn.addEventListener('click', e=>{
     calendar.classList.toggle('calendar-open')
+    const selectedDate = fromUnixTime(datePickerBtn.dataset.selectedDate)
+    setupDatePicker(selectedDate)
 })
 
+function setupDatePicker(selectedDate){
+   datePickerHeaderText.innerHTML = format(selectedDate, "MMMM - yyyy")
+}
+
 function setDate(date){
-    datePickerBtn.innerText = format(date, 'MMMM')
+    datePickerBtn.innerText = format(date, 'MMMM do, yyyy')
+    datePickerBtn.dataset.selectedDate = getUnixTime(date)
 }
 
 setDate(new Date())
